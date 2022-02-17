c4776ede4593a04b9dc3137ce6a80f37983143ee
tuanit168 <tuanit168@gmail.com>
2022-02-17 17:16:08 +0700

    Make API request to an url to fetch basic contents

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => console.log(users));
+  }
+
   render() {
     return (
       <div className="App">
